99146f72e1e8bf63340e4cc0a5497ebb56aa71ab
CODE-DEMON64 <CODE-DEMON64>
2023-03-22 12:19:30 -0700

    0-preprocessor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - A progrma that prints a line wuth puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzz1e")	
+;
+return (0);
+}
